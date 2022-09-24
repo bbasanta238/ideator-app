d85edd5fc4b33e7de4b4290b871e1d2c4f51a158
bbasanta238 <bbasanta238@gmail.com>
2022-09-24 12:11:05 +0545

    edit pathc

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -8,6 +8,28 @@ class IdeasController < ApplicationController
         redirect_to root_path
     end
 
+    def show
+        @idea = Idea.find(params[:id])
+    end
+
+    def edit
+        @idea = Idea.find(params[:id])
+
+    end
+    def update
+        @idea = Idea.find(params[:id])
+        if @idea.update(idea_params)
+          redirect_to root_path
+        else
+          redirect_to edit_idea_path(params[:id])
+        end
+    end
+    def destroy
+        @idea = Idea.find(params[:id])
+        @idea.destroy
+        redirect_to root_path
+      end
+
     private
 
     def idea_params

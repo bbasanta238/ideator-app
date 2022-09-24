3279ed835a4351ff3c59895c08027845333a6d02
bbasanta238 <bbasanta238@gmail.com>
2022-09-24 13:26:16 +0545

    adding about and random controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'about' => 'static_pages#about'
+  get 'random' => 'static_pages#random'
+
 
   # Defines the root path route ("/")
   # root "articles#index"

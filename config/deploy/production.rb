93f1fed53ed071c73408e0b5d5f7754da39a0e21
bbasanta238 <bbasanta238@gmail.com>
2024-09-04 18:41:23 +0545

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,8 +7,6 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
-
 # role-based syntax
 # ==================
 
@@ -21,8 +19,6 @@
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-
-
 # Configuration
 # =============
 # You can set any configuration variable like in config/deploy.rb
@@ -30,8 +26,6 @@
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
-
-
 
 # Custom SSH Options
 # ==================
@@ -59,3 +53,61 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+lock "~> 3.19.1"
+set :application, "ideator-app"
+set :repo_url, "https://github.com/bbasanta238/ideator-app.git"
+set :deploy_to, "/home/ubuntu/ideator-app"
+set :use_sudo, true
+set :branch, "main"
+set :linked_files, %w{config/master.key config/database.yml}
+set :rails_env, "production"
+set :keep_releases, 2
+set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
+set :linked_files, %w{config/database.yml config/master.key}
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log, "#{release_path}/log/puma.error.log"
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+namespace :puma do
+  desc "Create Directories for Puma Pids and Socket"
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  desc "Start Puma"
+  task :start do
+    on roles(:app) do
+      within release_path do
+        execute "bundle exec puma -C #{fetch(:puma_bind)} -e #{fetch(:rails_env)}"
+      end
+    end
+  end
+
+  desc "Stop Puma"
+  task :stop do
+    on roles(:app) do
+      execute "if test -f #{fetch(:puma_pid)}; then kill -s TERM `cat #{fetch(:puma_pid)}`; fi"
+    end
+  end
+
+  desc "Restart Puma"
+  task :restart do
+    on roles(:app) do
+      execute "if test -f #{fetch(:puma_pid)}; then kill -s USR1 `cat #{fetch(:puma_pid)}`; else #{fetch(:puma_bin)}; fi"
+    end
+  end
+
+  before :start, :make_dirs
+end

dd82fbc847a9ba2ab413c99db4212a6bc9f0f78d
bbasanta238 <bbasanta238@gmail.com>
2024-09-06 14:21:16 +0545

    change ip

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,8 @@
-# ... (keep the existing settings)
-
-# Add this at the top of your file, after the requires
-require "sshkit/sudo"
 lock "~> 3.19.1"
 
 set :application, "ideator"
 set :repo_url, "git@github.com:bbasanta238/ideator.git"
-set :deploy_to, "/var/www/ideator"
+set :deploy_to, "/home/ubuntu/ideator"
 set :rbenv_type, :user
 set :rbenv_ruby, "3.1.2"
 set :user, "ubuntu"
@@ -16,52 +12,24 @@ set :linked_files, %w{config/database.yml config/master.key}
 set :rails_env, "production"
 set :keep_releases, 2
 set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
-
-# ... (keep other existing settings)
-
-# Override the default deploy:check:directories task
-namespace :deploy do
-  namespace :check do
-    task :directories do
-      on roles(:all) do |host|
-        path = shared_path
-        sudo :mkdir, "-pv", path
-        sudo :chown, "#{host.user}:#{host.user}", path
-        path = releases_path
-        sudo :mkdir, "-pv", path
-        sudo :chown, "#{host.user}:#{host.user}", path
-      end
-    end
-  end
-end
-
-namespace :deploy do
-  desc "Create necessary directories"
-  task :create_directories do
-    on roles(:app) do |host|
-      path = fetch(:deploy_to)
-      sudo :mkdir, "-pv", path
-      sudo :chown, "-R", "#{host.user}:#{host.user}", path
-    end
-  end
-
-  before "deploy:check:directories", :create_directories
-end
-
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log, "#{release_path}/log/puma.error.log"
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 namespace :puma do
   desc "Create Directories for Puma Pids and Socket"
   task :make_dirs do
-    on roles(:app) do |host|
-      path = "#{shared_path}/tmp/sockets"
-      sudo :mkdir, "-pv", path
-      sudo :chown, "-R", "#{host.user}:#{host.user}", path
-      path = "#{shared_path}/tmp/pids"
-      sudo :mkdir, "-pv", path
-      sudo :chown, "-R", "#{host.user}:#{host.user}", path
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
   before :start, :make_dirs
 end
-
-# Add this at the end of your file
-set :pty, true
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,9 @@
-server "13.230.249.171", user: "ubuntu", roles: %w{web app db}
+server "10.0.22.47", user: "ubuntu", roles: %w{app web db} # AZ-a
 set :ssh_options, {
+  keys: %w(~/.ssh/re_ssh.pem),
   forward_agent: true,
-  auth_methods: %w[publickey],
-  keys: %w[~/.ssh/re_ssh.pem],
+  auth_methods: %w(publickey),
+  proxy: Net::SSH::Proxy::Command::new("ssh bastion-host -W %h:%p"),
   verbose: :debug,
   verify_host_key: :never,
 }

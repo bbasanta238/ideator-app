2c54681bfaffca2cc7440684ce64c97fb3a6bcf6
bbasanta238 <bbasanta238@gmail.com>
2024-09-06 11:33:26 +0545

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,7 @@
+# ... (keep the existing settings)
+
+# Add this at the top of your file, after the requires
+require "sshkit/sudo"
 lock "~> 3.19.1"
 
 set :application, "ideator"
@@ -13,35 +17,51 @@ set :rails_env, "production"
 set :keep_releases, 2
 set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
 
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.access.log"
-set :puma_error_log, "#{release_path}/log/puma.error.log"
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
+# ... (keep other existing settings)
+
+# Override the default deploy:check:directories task
+namespace :deploy do
+  namespace :check do
+    task :directories do
+      on roles(:all) do |host|
+        path = shared_path
+        sudo :mkdir, "-pv", path
+        sudo :chown, "#{host.user}:#{host.user}", path
+        path = releases_path
+        sudo :mkdir, "-pv", path
+        sudo :chown, "#{host.user}:#{host.user}", path
+      end
+    end
+  end
+end
 
 namespace :deploy do
   desc "Create necessary directories"
   task :create_directories do
-    on roles(:app) do
-      sudo :mkdir, "-p", "/var/www/#{fetch(:application)}"
-      sudo :chown, "-R", "#{fetch(:user)}:#{fetch(:user)}", "/var/www/#{fetch(:application)}"
+    on roles(:app) do |host|
+      path = fetch(:deploy_to)
+      sudo :mkdir, "-pv", path
+      sudo :chown, "-R", "#{host.user}:#{host.user}", path
     end
   end
 
-  before "deploy:check:linked_dirs", :create_directories
+  before "deploy:check:directories", :create_directories
 end
 
 namespace :puma do
   desc "Create Directories for Puma Pids and Socket"
   task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
+    on roles(:app) do |host|
+      path = "#{shared_path}/tmp/sockets"
+      sudo :mkdir, "-pv", path
+      sudo :chown, "-R", "#{host.user}:#{host.user}", path
+      path = "#{shared_path}/tmp/pids"
+      sudo :mkdir, "-pv", path
+      sudo :chown, "-R", "#{host.user}:#{host.user}", path
     end
   end
   before :start, :make_dirs
 end
+
+# Add this at the end of your file
+set :pty, true
